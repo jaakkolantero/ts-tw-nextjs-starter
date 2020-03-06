819b6edf4710c00e86bc02dd938d0a5fe2b8f851
UFo_x <tero.jaakkola1@gmail.com>
2020-03-06 21:13:28 +0200

    convert to typescript. add npm scripts.

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+export default () => (
+  <div className="flex min-h-screen flex-col">
+    <div className="flex-1 py-12 px-10 bg-gray-100">content</div>
+    <div className="bg-black text-white py-12 px-10">footer</div>
+  </div>
+);
